547e43325b4afc48d2cfcf842d6736b6a514255c
Ian-Blank <ianblank3@gmail.com>
2023-05-28 18:47:52 +0200

    Made the Generator, had issues with edge cases. But that is fixed do not know if each puzzle generated has a unique sollution.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+
 int[] collumn = new int[] {3,0,0,2,0,2};
 int[] allrows = new int[] { 2, 0, 2, 1, 1, 1 };
 int[,] grid1 = new int[,] 
@@ -12,8 +16,14 @@ int[,] grid1 = new int[,]
 
 Console.WriteLine("Please enter size of the puzzle: ");
 int size = int.Parse(Console.ReadLine());
-Tents puzzle = new Tents(grid1,collumn,allrows);
-puzzle.display();
+Console.WriteLine("Please enter amount of tents in the puzzle: ");
+int tents = int.Parse(Console.ReadLine());
+for (int i = 0; i < 1; i++)
+{
+    Tents puzzle = new Tents(size, tents);
+    puzzle.display();
+}
+
 
 class Tents
 {
@@ -21,13 +31,79 @@ class Tents
     int[] rows;
     int[] cols;
 
-    public Tents(int size)
+    public Tents(int size, int tents)
     {
         grid = new int[size,size];
         rows = new int[size]; 
         cols = new int[size];
+
+        Random rdn = new Random();
+        int x = rdn.Next(0,size);
+        int y = rdn.Next(0,size);
+        int limit = 1000;
+        while (tents > 0)
+        {
+            if (grid[x, y] == 0)
+            {
+                
+                int randomBool = rdn.Next(2);
+                int tent = rdn.Next(3) - 1;
+                while (tent == 0)
+                {
+                    tent = rdn.Next(3) - 1;
+                }
+               
+                if (randomBool == 0 && tentcheck(x + tent, y) && inGrid(x+tent,y))
+                {
+                    grid[x, y] = 3;
+                    grid[x + tent, y] = 1;
+                    rows[x + tent]++;
+                    cols[y]++;
+                    tents--;
+                }
+                else if (tentcheck(x, y + tent) && inGrid(x, y + tent))
+                {
+                    grid[x, y] = 3;
+                    grid[x, y + tent] = 1;
+                    rows[x]++;
+                    cols[y+ tent]++;
+                    tents--;
+
+                }
+                x = rdn.Next(size);
+                y = rdn.Next(size);
+            }
+            else
+            {
+                if (x + 1 < grid.GetLength(0))
+                    x++;
+                else if (y + 1 < grid.GetLength(0))
+                    y++;
+            }
+            limit--;
+        }
+
+
     }
 
+    private bool tentcheck(int x, int y)
+    {
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+            {
+                if (inGrid(x+i,y+j))
+                    if (grid[x + i, y + j] == 1)
+                        return false;
+            }
+        return true;
+    }
+
+    private bool inGrid(int x,int y)
+    {
+        if (x < grid.GetLength(0) && x >= 0 && y < grid.GetLength(0) && y >= 0)
+            return true;
+        return false;
+    }
 
     public Tents(int[,] puzzle, int[] col, int[] row)
     {
@@ -38,24 +114,31 @@ class Tents
 
     public void display()
     {
-        Console.WriteLine("  " + string.Join(" ", cols));
+        Console.WriteLine(" " + string.Join("", cols));
         for (int i = 0; i < rows.Length; i++)
         {
             Console.Write(rows[i]);
             for (int j = 0; j < cols.Length; j++)
             {
-                Console.Write("|");
+                //Console.Write("|");
                 switch (grid[i,j])
                 {
                     case 0:
                         Console.ForegroundColor = ConsoleColor.Green;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.BackgroundColor = ConsoleColor.Red;
                         break;
                     case 3:
                         Console.ForegroundColor= ConsoleColor.DarkYellow;
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
                         break;
                 }
                 Console.Write(this.grid[i, j]);
                 Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
             }
             Console.Write("\n");
         }
